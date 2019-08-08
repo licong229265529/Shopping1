9ef872ad3ededf0b24a170bf463b6518a9f9a7b7
zhangyuchen886 <3444769311@qq.com>
2019-08-08 16:35:39 +0800

    zyc

--- app/src/main/java/com/example/shopping/fragments/HomekFragment.java
+++ app/src/main/java/com/example/shopping/fragments/HomekFragment.java
@@ -1,13 +1,25 @@
 package com.example.shopping.fragments;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.shopping.R;
+import com.example.shopping.CaptureActivity;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,16 +27,74 @@ import com.example.shopping.R;
 public class HomekFragment extends Fragment {
 
 
+    private View view;
+    private ImageView mEwm;
+    private static final int REQUEST_CODE_SCAN = 0x0000;
+    private static final String DECODED_CONTENT_KEY = "codedContent";
+    private static final String DECODED_BITMAP_KEY = "codedBitmap";
     public HomekFragment() {
         // Required empty public constructor
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homek, container, false);
+        View inflate = inflater.inflate(R.layout.fragment_homek, null);
+        initView(inflate);
+        return inflate;
+    }
+
+    private void initView(View inflate) {
+        mEwm = (ImageView) inflate.findViewById(R.id.ewm);
+        mEwm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //动态权限申请
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, 1);
+                } else {
+                    goScan();
+                }
+            }
+        });
+    }
+
+    private void goScan() {
+        Toast.makeText(getActivity(), "扫描二维码", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_SCAN);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    goScan();
+                } else {
+                    Toast.makeText(getActivity(), "你拒绝了权限申请，可能无法打开相机扫码哟！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) { //1
+                //返回的文本内容
+                String content = data.getStringExtra(DECODED_CONTENT_KEY);
+                //返回的BitMap图像
+                Bitmap bitmap = data.getParcelableExtra(DECODED_BITMAP_KEY);
+
+                Toast.makeText(getActivity(), "你扫描到的内容是："+content, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
