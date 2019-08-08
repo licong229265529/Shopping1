6a578b9d255608b4c1afdc5c5cf971d78085b532
licong229265529 <a229265529>
2019-08-08 16:34:12 +0800

    cong1

--- app/src/main/java/com/example/shopping/fragments/ShopFragment.java
+++ app/src/main/java/com/example/shopping/fragments/ShopFragment.java
@@ -15,16 +15,11 @@ import com.example.shopping.R;
 public class ShopFragment extends Fragment {
 
 
-    public ShopFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        View view = inflater.inflate(R.layout.fragment_shop, null);
+        return view;
     }
 
 }
